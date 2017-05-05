22575b1d86047328e674ae9837b8736a1bb3e599
Kriz Cortes <hire@krizcortes.com>
2017-05-04 19:12:11 -0700

    Initial Board Component Centered

--- react-client/src/components/Board.js
+++ react-client/src/components/Board.js
@@ -1,5 +1,38 @@
-/*
-  
-  Board component ( under players, blocks, boxes ) bottom layer ( sibling on rendering container )
+/* Board component ( under players, blocks, boxes ) bottom layer ( sibling on rendering container ) */
+import React from 'react';
 
-*/
+class Board extends React.Component {
+
+  constructor(props){
+    super(props);
+    // this.state { }
+  }
+
+  componentDidMount(){
+    let height = $(document).height();
+    let width = $(document).width();
+
+    let Board = $('#Board');
+
+    let boardHeight = 568;
+    let boardWidth = 320;
+
+    let topmargin = ((height - boardHeight) / 2);
+    let leftMargin = ((width - boardWidth) / 2);
+
+    $(Board).css({ top: topmargin, left: leftMargin });
+
+  }
+
+  render(){
+    return (
+
+        <div id="Board">
+          BOARD
+        </div>
+    );
+  }
+
+}
+
+export default Board;
--- react-client/src/components/Game.js
+++ react-client/src/components/Game.js
@@ -1,5 +1,39 @@
-/*
-  
-  Main container for the game
+/* Main container for the game */
 
-*/
+import React from 'react';
+import Board from './Board.js';
+
+class Game extends React.Component {
+
+  constructor(props){
+    super(props);
+
+  }
+
+  componentDidMount(){
+    let height = $(document).height();
+    let width = $(document).width();
+
+    let Game = $('#Game');
+
+    $(Game).css({ width: width,
+                  height: height,
+                  minWidth: width, 
+                  minHeight: height
+                });
+
+  }
+
+
+  render(){
+    return( 
+        <div id="Game">
+          <Board />
+        </div>
+
+    );
+  }
+
+}
+
+export default Game;
