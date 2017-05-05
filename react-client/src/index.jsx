26896813125f407d9b3bfc8aedd41d63e8b98de7
Hack Reactor Student <student@hackreactor.com>
2017-05-04 20:12:46 -0700

    Implement room feature

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -5,13 +5,154 @@ import $ from 'jquery';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      test: '',
+      socket: () => {},
+      clientID: '',
+      room: ''
+    };
+
+    this.newGame1OnClick = this.newGame1OnClick.bind(this);
+    this.newGame2OnClick = this.newGame2OnClick.bind(this);
+    this.joinGame1OnClick = this.joinGame1OnClick.bind(this);
+    this.joinGame2OnClick = this.joinGame2OnClick.bind(this);
+    this.test = this.test.bind(this);
+  }
+
+  newGame1OnClick () {
+    var socket = io.connect('/');
+    socket.on('onconnected', (data) => {
+      console.log('connected successfuly to the socket.io server. My server side ID is ', data.id);
+      socket.emit('create', 'room1');
+    });
+
+    socket.on('room info', ({clientID, adapter}) => {
+      console.log("clientID: ",clientID); //client id
+      console.log("adapter: ",adapter); //data.adapter.room1 || data.adapter.room2
+      this.setState({
+        socket: socket,
+        room: 'room1',
+        clientID: clientID
+      })
+    });
+
+    socket.on('get test', ({test, room}) => {
+      if (room === this.state.room){
+        this.setState({
+          test: test
+        })
+      }
+    });
+
+  }
+
+  newGame2OnClick () {
+    var socket = io.connect('/');
+    socket.on('onconnected', (data) => {
+      console.log('connected successfuly to the socket.io server. My server side ID is ', data.id);
+      socket.emit('create', 'room2');
+    });
+
+    socket.on('room info', ({clientID, adapter}) => {
+      console.log("clientID: ",clientID); //client id
+      console.log("adapter: ",adapter); //data.adapter.room1 || data.adapter.room2
+      this.setState({
+        socket: socket,
+        room: 'room2',
+        clientID: clientID
+      })
+    });
+
+    socket.on('get test', ({test, room}) => {
+      if (room === this.state.room){
+        this.setState({
+          test: test
+        })
+      }
+    });
+  }
+
+  joinGame1OnClick () {
+    var socket = io.connect('/');
+    socket.on('onconnected', (data) => {
+      console.log('connected successfuly to the socket.io server. My server side ID is ', data.id);
+      socket.emit('join', 'room1');
+    });
+
+    socket.on('room info', ({clientID, adapter}) => {
+      console.log("clientID: ",clientID); //client id
+      console.log("adapter: ",adapter); //data.adapter.room1 || data.adapter.room2
+      this.setState({
+        socket: socket,
+        room: 'room1',
+        clientID: clientID
+      })
+    });
+
+    socket.on('get test', ({test, room}) => {
+      if (room === this.state.room){
+        this.setState({
+          test: test
+        })
+      }
+    });
+  }
+
+  joinGame2OnClick () {
+    var socket = io.connect('/');
+    socket.on('onconnected', (data) => {
+      console.log('connected successfuly to the socket.io server. My server side ID is ', data.id);
+      socket.emit('join', 'room2');
+    });
+
+    socket.on('room info', ({clientID, adapter}) => {
+      console.log("clientID: ",clientID);
+      console.log("adapter: ",adapter);
+      this.setState({
+        socket: socket,
+        room: 'room2',
+        clientID: clientID
+      })
+    });
+
+    socket.on('get test', ({test, room}) => {
+      if (room === this.state.room){
+        this.setState({
+          test: test
+        })
+      }
+    });
+  }
+
+  test() {
+    console.log('click');
+    this.state.socket.emit('send test', {
+      test: 'HELL YEAH I GET IT!!!',
+      room: this.state.room
+    });
+    this.setState({
+      test: 'HELL YEAH I GET IT!!!'
+    })
   }
 
   render() {
     return (
     <div>
-      Bomber Man!
+      <button onClick={this.newGame1OnClick}> New Game room 1</button>
+      <button onClick={this.newGame2OnClick}> New Game room 2</button>
+      <br></br>
+      <button onClick={this.joinGame1OnClick}> Join Game room 1</button>
+      <button onClick={this.joinGame2OnClick}> Join Game room 2</button>
+      <br></br>
+      <button onClick={this.test}> test </button>
+
+      <div id='test'>
+        {this.state.test}
+      </div>
+      <div>
+        <p>{JSON.stringify(this.state.clientID)}</p>
+        <p>{this.state.room.toString()}</p>
+      </div>
     </div>)
   }
 }
--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ sio.use(function(socket, next) {
 //Socket.io will call this function when a client connects, 
 //So we can send that client a unique ID we use so we can 
 //maintain the list of players.
-sio.sockets.on('connection', function (client) {
+sio.sockets.on('connection', (client) => {
   //Generate a new UUID, looks something like 
   //5b2ca132-64bd-4513-99da-90e838ca47d1
   //and store this on their socket/connection
@@ -53,11 +53,36 @@ sio.sockets.on('connection', function (client) {
   console.log('\t socket.io:: player ' + client.userid + ' connected');
   
   //When this client disconnects
-  client.on('disconnect', function () {
+  client.on('disconnect', () => {
     //Useful to know when someone disconnects
     console.log('\t socket.io:: client disconnected ' + client.userid );
   }); //client.on disconnect
   
+
+
+  client.on('create', (room) => {
+    client.join(room);
+    client.emit('room info', {
+      clientID: client.rooms,
+      adapter: client.adapter.rooms
+    });
+  });
+
+  client.on('join', (room) => {
+    client.join(room);
+    client.emit('room info', {
+      clientID: client.rooms,
+      adapter: client.adapter.rooms
+    });
+  })
+
+  client.on('send test', ({test, room}) => {
+    console.log('send test');
+    client.broadcast.emit('get test', {
+      test: test,
+      room: room
+    });
+  })
 }); //sio.sockets.on connection
 
 
